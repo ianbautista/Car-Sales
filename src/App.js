6820b96d8f67847ed9885490de2c0b9875f97621
ianbautista <ianfbautista@gmail.com>
2020-08-11 14:20:02 -0700

    Added events and event handlers in App. Build action type and action creators in action.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ import Total from "./components/Total";
 import { connect } from "react-redux";
 
 const App = (props) => {
+	const removeFeature = (feature) => {
+		props.removeFeature(feature);
+	};
+
+	const addFeature = (feature) => {
+		props.addFeature(feature);
+	};
+
 	return (
 		<div className="boxes">
 			<div className="box">
 				<Header car={props.car} />
-				<AddedFeatures car={props.car} />
+				<AddedFeatures car={props.car} removeFeature={removeFeature} />
 			</div>
 			<div className="box">
-				<AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+				<AdditionalFeatures
+					additionalFeatures={props.additionalFeatures}
+					addFeature={addFeature}
+				/>
 				<Total car={props.car} additionalPrice={props.additionalPrice} />
 			</div>
 		</div>
